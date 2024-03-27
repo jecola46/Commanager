bc06c38591fb216fd130664ae6fe0296b6672add
Aul <houseemdee@yahoo.com>
2024-03-26 17:14:28 -0700

    Refactored deck display canvas

--- ui_elements/deck_lister.py
+++ ui_elements/deck_lister.py
@@ -1,91 +1,71 @@
 import tkinter as tk
-from .deck_viewer import DeckViewer
+from PIL import Image, ImageTk
+from deck_io import grab_card_art_async
 
-class DeckLister(tk.Frame):
-    def __init__(self, root, deck_collection):
-        super().__init__(root, None)
+class DeckLister(tk.Canvas):
+    def __init__(self, root, deck_lists, deck_collection):
+        super().__init__(root)
         self.root = root
         self.root.title("Deck Viewer")
+        self.deck_lists = deck_lists
         self.deck_collection = deck_collection
 
-        self.create_deck_listbox()
+        self.create_deck_collection()
+
+    def create_deck_collection(self):
+        self.deck_image_labels = []
         
+        self.bind_all("<MouseWheel>", self.on_mousewheel)
+        self.set_scrollbar()
 
-    def create_deck_listbox(self):
-        # Create a listbox to display decks
-        self.deck_listbox = tk.Listbox(self.root, selectmode=tk.MULTIPLE, height=65, width=80)
-        self.deck_listbox.grid(row=1, column=2, pady=10, sticky='w', rowspan=5)
+        # Create a frame to contain the deck boxes
+        deck_collection_frame = tk.Frame(self)
+        self.create_window((0, 0), window=deck_collection_frame, anchor='nw')
 
-        # Populate the listbox with deck names
-        self.populate_deck_list()
+        self.grid_rowconfigure(1, weight=1)
 
-        # Bind the listbox selection to display deck details
-        self.deck_listbox.bind('<<ListboxSelect>>', self.display_deck_details)
+        for column in range(0, 7):
+            deck_collection_frame.grid_columnconfigure(column, weight=1, uniform="group1")
 
-    def populate_deck_list(self):
-        # Clear existing items in the listbox
-        self.deck_listbox.delete(0, tk.END)
+        # Create a grid for displaying decks
+        for i, deck in enumerate(self.deck_lists):
+            image_path = deck.get('local_image_path', 'resources\\no-photo.png')
 
-        # Get the deck names from the collection
-        decks = self.deck_collection.get_deck_summaries()
-        self.decks = []
+            # Calculate row and column index
+            row = i // 7
+            column = i % 7
 
-        # Populate the listbox with deck names
-        for deck in decks:
-            self.deck_listbox.insert(tk.END, deck['name'])
-            self.decks.append(deck)
+            # Load image and resize it to desired dimensions
+            image = Image.open(image_path)
+            image = image.resize((212, 156))
+            photo = ImageTk.PhotoImage(image)
 
-    def display_deck_details(self, event):
-        self.viewing_deck_index = self.deck_listbox.curselection()[0]
-        deck = self.decks[self.viewing_deck_index]
-        self.show_deck_viewer(deck)
+            deck_box_frame = tk.Frame(deck_collection_frame)
+            deck_box_frame.grid(row=row, column=column, padx=10, pady=5)
 
-    def show_next_deck(self):
-        if self.viewing_deck_index < len(self.decks) - 1:
-            self.viewing_deck_index += 1
-            deck = self.decks[self.viewing_deck_index]
-            self.show_deck_viewer(deck)
+            # Create label to display image
+            deck_image_label = tk.Label(deck_box_frame, image=photo)
+            deck_image_label.card_art_name = self.deck_collection.get_deck_commander_name(deck['publicId'])
+            deck_image_label.image = photo  # Keep reference to avoid garbage collection
+            deck_image_label.pack(side = tk.TOP, padx=5)
+            self.deck_image_labels.append(deck_image_label)
 
-    def show_deck_viewer(self, deck):
-        self.destroy_widgets()
+            # Display deck name
+            deck_name = deck.get('name', 'Unknown Deck').replace('[Owned]', '')
+            # Shorten long deck names
+            deck_name = (deck_name[:36] + '...') if len(deck_name) > 39 else deck_name
 
-        # Create and place the DeckViewer instance
-        deck_viewer = DeckViewer(self.root, self.deck_collection, deck, self.back_to_deck_lister, self.show_next_deck)
-        deck_viewer.grid(row=1, column=2, pady=10, sticky='w', rowspan=5)
+            deck_name_label = tk.Label(deck_box_frame, text=deck_name, font=("Helvetica", 12))
+            deck_name_label.pack(side = tk.TOP, padx=2)
 
-    def back_to_deck_lister(self):
-        self.create_deck_listbox()
+        grab_card_art_async(self.deck_image_labels)
 
-    def destroy_widgets(self):
-        self.deck_listbox.destroy()
+    def set_scrollbar(self):
+        # Bind scrollbar to canvas
+        self.bind("<Configure>", self.on_canvas_configure)
 
-    def highlight_decks(self, decks):
-        for x in range(len(self.decks)):
-            deck = self.decks[x]
-            if deck['publicId'] in decks:
-                self.deck_listbox.select_set(x)
+    def on_canvas_configure(self, event):
+        self.configure(scrollregion=self.bbox("all"))
 
-    def list_decks_with_num(self, decks_and_count):
-        # Clear existing items in the listbox
-        self.deck_listbox.delete(0, tk.END)
-        self.decks = []
-
-        # Populate the listbox with deck names
-        for deck, count in decks_and_count:
-            self.deck_listbox.insert(tk.END, deck + f' {count}')
-            self.decks.append(deck)
-
-    def filter_decks_by_color(self, filter_set):
-        # Clear existing items in the listbox
-        self.deck_listbox.delete(0, tk.END)
-        new_decks = []
-
-        deck_colors_map = self.deck_collection.get_deck_colors_map()
-
-        for deck in self.decks:
-            deck_id = deck['publicId']
-            if filter_set.issubset(deck_colors_map[deck_id]):
-                self.deck_listbox.insert(tk.END, deck['name'])
-                new_decks.append(deck)
-
-        self.decks = new_decks
+    def on_mousewheel(self, event):
+        self.yview_scroll(-1 * int(event.delta/120), "units")
--- ui_elements/main_app_ui.py
+++ ui_elements/main_app_ui.py
@@ -1,7 +1,6 @@
 import tkinter as tk
-from PIL import Image, ImageTk
 import math
-from deck_io import grab_card_art_async
+from .deck_lister import DeckLister
 
 class MainAppUI(tk.Tk):
     def __init__(self, deck_collection):
@@ -16,71 +15,19 @@ class MainAppUI(tk.Tk):
         deck_list_label = tk.Label(self, text="Your decks", font=("Helvetica", 24))
         deck_list_label.grid(row=0, column=1, pady=10, sticky='ew')
 
+        self.create_filter_ui()
+
         self.grid_columnconfigure(1, weight=1)
-
-        self.deck_image_labels = []
-
-        # Create a canvas for scrollable area
-        self.canvas = tk.Canvas(self)
-        self.canvas.grid(row=1, column=1, sticky='nsew')
-        self.canvas.bind_all("<MouseWheel>", self.on_mousewheel)
-
-        # Add a scrollbar
-        scrollbar = tk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
-        scrollbar.grid(row=1, column=2, sticky='ns')
-        self.canvas.configure(yscrollcommand=scrollbar.set)
-
-        # Create a frame to contain the deck boxes
-        deck_collection_frame = tk.Frame(self.canvas)
-        self.canvas.create_window((0, 0), window=deck_collection_frame, anchor='nw')
-
-        # Bind scrollbar to canvas
-        def on_canvas_configure(event):
-            self.canvas.configure(scrollregion=self.canvas.bbox("all"))
-
-        # Bind scrollbar to canvas
-        self.canvas.bind("<Configure>", self.on_canvas_configure)
-
         self.grid_rowconfigure(1, weight=1)
 
-        for column in range(0, 7):
-            deck_collection_frame.grid_columnconfigure(column, weight=1, uniform="group1")
+        deck_list = self.deck_collection.get_deck_summaries()
+        self.deck_lister = DeckLister(self, deck_list, self.deck_collection)
+        self.deck_lister.grid(row=1, column=1, sticky='nsew')
 
-        # Create a grid for displaying decks
-        for i, deck in enumerate(self.deck_collection.get_deck_summaries()):
-            image_path = deck.get('local_image_path', 'resources\\no-photo.png')
+        # Add a scrollbar
+        scrollbar = tk.Scrollbar(self, orient="vertical", command=self.deck_lister.yview)
+        scrollbar.grid(row=1, column=2, sticky='ns')
+        self.deck_lister.configure(yscrollcommand=scrollbar.set)
 
-            # Calculate row and column index
-            row = i // 7
-            column = i % 7
-
-            # Load image and resize it to desired dimensions
-            image = Image.open(image_path)
-            image = image.resize((212, 156))
-            photo = ImageTk.PhotoImage(image)
-
-            deck_box_frame = tk.Frame(deck_collection_frame)
-            deck_box_frame.grid(row=row, column=column, padx=10, pady=5)
-
-            # Create label to display image
-            deck_image_label = tk.Label(deck_box_frame, image=photo)
-            deck_image_label.card_art_name = self.deck_collection.get_deck_commander_name(deck['publicId'])
-            deck_image_label.image = photo  # Keep reference to avoid garbage collection
-            deck_image_label.pack(side = tk.TOP, padx=5)
-            self.deck_image_labels.append(deck_image_label)
-
-            # Display deck name
-            deck_name = deck.get('name', 'Unknown Deck').replace('[Owned]', '')
-            # Shorten long deck names
-            deck_name = (deck_name[:36] + '...') if len(deck_name) > 39 else deck_name
-
-            deck_name_label = tk.Label(deck_box_frame, text=deck_name, font=("Helvetica", 12))
-            deck_name_label.pack(side = tk.TOP, padx=5)
-
-        grab_card_art_async(self.deck_image_labels)
-
-    def on_canvas_configure(self, event):
-        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
-
-    def on_mousewheel(self, event):
-        self.canvas.yview_scroll(-1 * int(event.delta/120), "units")
+    def create_filter_ui(self):
+        pass
